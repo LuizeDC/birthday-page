2ef382853b1903127e9fb6db2b3c3d926447980f
Luize Dalla Costa <96740505+LuizeDC@users.noreply.github.com>
2023-03-03 15:11:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ function clickMe() {
 
 let clickMeButton = document.querySelector("button");
 clickMeButton.addEventListener("click", clickMe);
+
+function learnName() {
+  let name = prompt("What is your first name?");
+  let heading = document.querySelector("h1");
+  heading.innerHTML = "Hi " + name + "!";
+}
+
+learnName();
